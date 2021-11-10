1091443fa70f6a9352d33bf6b72c64f4299c6191
Carla <CMontano@greenmountaintechnology.com>
2021-11-10 15:04:37 -0600

    creatin an activity list

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,20 +1,17 @@
 import React from "react";
 import { Grid, GridColumn, List, ListItem } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
 
 interface Props {
     activities: Activity[];
 }
 
-export default function ActivityDashboard(props: Props) {
+export default function ActivityDashboard({activities}: Props) {
     return (
         <Grid>
             <GridColumn width='10'>
-            <List>
-                {props.activities.map(activity => (
-                    <ListItem key={activity.id}>{activity.title}</ListItem>
-                ))}
-            </List>
+                <ActivityList activities={activities} />
             </GridColumn>
         </Grid>
     )
