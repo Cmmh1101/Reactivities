134250fdb5340881840e62f47386e3c964a05ea3
Carla <CMontano@greenmountaintechnology.com>
2021-12-28 10:28:46 -0600

    end of styling the UI

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,4 +1,3 @@
-import { SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import {
   Button,
@@ -12,25 +11,12 @@ import {
   SegmentGroup,
 } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
-import { useStore } from "../../../app/stores/store";
 
 interface Props {
   activity: Activity;
 }
 
 export default function ActivityListItem({ activity }: Props) {
-  const { activityStore } = useStore();
-  const { deleteActivity } = activityStore;
-
-  const [target, setTarget] = useState("");
-
-  function handleActivityDelete(
-    e: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-  }
   return (
     <SegmentGroup>
       <Segment>
@@ -65,37 +51,4 @@ export default function ActivityListItem({ activity }: Props) {
       </Segment>
     </SegmentGroup>
   );
-}
-
-{
-  /* <Item key={activity.id}>
-      <ItemContent>
-        <ItemHeader as="a">{activity.title}</ItemHeader>
-        <ItemMeta>{activity.date}</ItemMeta>
-        <ItemDescription>
-          <div>{activity.description}</div>
-          <div>
-            {activity.city}, {activity.venue}
-          </div>
-        </ItemDescription>
-        <ItemExtra>
-          <Button
-            as={Link}
-            to={`/activities/${activity.id}`}
-            floated="right"
-            content="View"
-            color="blue"
-          />
-          <Button
-            name={activity.id}
-            loading={loading && target === activity.id}
-            onClick={(e) => handleActivityDelete(e, activity.id)}
-            floated="right"
-            content="Delete"
-            color="red"
-          />
-          <Label basic content={activity.category} />
-        </ItemExtra>
-      </ItemContent>
-    </Item> */
 }
