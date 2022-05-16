31866abe792c4b44611ae29409e9b9d201e65af9
Carla <CMontano@greenmountaintechnology.com>
2022-05-16 17:19:58 -0500

    login local storage persistence, initial login modal work

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,8 +1,23 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { Button, Container, Menu, MenuItem } from "semantic-ui-react";
+import { Link, NavLink } from "react-router-dom";
+import {
+  Button,
+  Container,
+  Menu,
+  MenuItem,
+  Image,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+} from "semantic-ui-react";
+import { useStore } from "../stores/store";
 
-export default function NavBar() {
+export default observer(function NavBar() {
+  const {
+    userStore: { user, logout },
+  } = useStore();
+
   return (
     <Menu inverted fixed="top">
       <Container>
@@ -24,7 +39,25 @@ export default function NavBar() {
             content="Create Activity"
           />
         </MenuItem>
+        <MenuItem position="right">
+          <Image
+            src={user?.image || "/assets/user.png"}
+            avatar
+            spaced="right"
+          />
+          <Dropdown pointing="top left" text={user?.displayName}>
+            <DropdownMenu>
+              <DropdownItem
+                as={Link}
+                to={`/profile/${user?.username}`}
+                text="My Profile"
+                icon="user"
+              />
+              <DropdownItem onClick={logout} text="Logout" icon="power" />
+            </DropdownMenu>
+          </Dropdown>
+        </MenuItem>
       </Container>
     </Menu>
   );
-}
+});
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,7 +15,7 @@ export default observer(function ActivityDashboard() {
   }, [activityRegistry.size, loadActivities]);
 
   if (activityStore.loadingInitial)
-    return <LoadingComponent content="Loadding app" />;
+    return <LoadingComponent content="Loadding activities" />;
 
   return (
     <Grid>
